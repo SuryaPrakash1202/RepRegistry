84a465452d4d663dc82ba8c4401612eb07f0ebcf
user name <kalathursuryaprakash2002@gmail.com>
2023-03-09 22:44:57 +0530

    Initial

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+const { default: mongoose } = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+//app.get("/", (req, res) => {res.send("hgjgujvhjb");});
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/workouts", workoutRoutes);
+
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/WorkOut?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2"
+);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Connection done...");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("listeneing on port", process.env.PORT);
+});
